c1520dbefe924dcbe6a210b60552a042ce8de95d
IsisMarieli <Isismarieli1@gmail.com>
2025-01-16 11:04:03 -0300

    métodos array

---
+++ script.js
@@ -0,0 +1,9 @@
+const arrayVazia = [,,,];
+
+arrayVazia.push(50)
+console.log(arrayVazia.length)
+
+console.log(arrayVazia)
+console.log(arrayVazia[0])
+console.log(arrayVazia[1])
+console.log(arrayVazia[2])
